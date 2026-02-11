c93a2f9dddc2ee327f554d76368345686c22a80e
Lucas <lucasgsantos1727@gmail.com>
2026-02-11 17:22:14 -0300

    criando os módulos de usuário e post com nest generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  doLogin(){
+  doLogin() {
     return 'Olá do authService';
   }
 }
